1acd98bc6249ac8176055d11240396d13dc04c44
harpreet221295 <harpreet.singh.cse14@itbhu.ac.in>
2017-11-12 20:54:50 +0530

    mod test file

--- test.py
+++ test.py
@@ -4,7 +4,6 @@
 import tweet_clean as tc
 import re
 
-
 str1 = u'इंदापूर तालुक्यातील भवानीनगरच्य श्र छत्रपत सहकार साखर कारखान्याच १८ मेगावॅट क्षमतेच सहवीज निर्मि प्रकल्प व'
 
 print tc.clean_tweet(str1)
