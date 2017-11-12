fde879cb46df6371131eae32eaeb5f3b147c2365
harpreet221295 <harpreet.singh.cse14@itbhu.ac.in>
2017-11-12 20:52:16 +0530

    added test file

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import tweet_clean as tc
+import re
+
+
+
+str1 = u'इंदापूर तालुक्यातील भवानीनगरच्य श्र छत्रपत सहकार साखर कारखान्याच १८ मेगावॅट क्षमतेच सहवीज निर्मि प्रकल्प व'
+
+print tc.clean_tweet(str1)
+#print tc.clean_tweet(str2.decode('utf-8'))
